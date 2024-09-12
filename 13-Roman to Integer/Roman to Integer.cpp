a9918ce67ddef99c87d46d0c344e4c8458bf848e
JLandqvi <johan.landqvist@analog.com>
2024-09-12 11:15:17 +0200

    13-Roman to Integer started

---
+++ 13-Roman to Integer/Roman to Integer.cpp
@@ -0,0 +1,27 @@
+#include <map>
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        std::map<char, int> M;
+        int num = 0;
+
+        M['I'] = 1;
+        M['V'] = 5;
+        M['X'] = 10;
+        M['L'] = 50;
+        M['C'] = 100;
+        M['D'] = 500;
+        M['M'] = 1000;
+
+        for(int i = s.length(); i > 0; i--){
+            if( M[s[i]] < M[s[i-1]] ){
+                num = num + M[s[i]];
+            }
+            if( M[s[i]] > M[s[i-1]] ){
+                num = num - M[s[i-1]];
+            }
+        }
+    return num;
+    }
+};
